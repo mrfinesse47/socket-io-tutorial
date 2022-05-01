b0e5c42452a7fc8886ba5620ef7bae9fecbb7451
Kevin Mason <krmason2020@gmail.com>
2022-05-01 13:53:50 -0700

    server is responding using sockets now

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,13 @@
 const socket = io();
 
+//to prevent default if you want
+
+// document
+//   .getElementById("chat-form")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault();
+//   });
+
 socket.on("message", (msg) => {
   console.log(msg);
 });
--- server.js
+++ server.js
@@ -13,8 +13,18 @@ app.use(express.static(path.join(__dirname, "public")));
 
 //run when a client connects
 io.on("connection", (socket) => {
-  console.log("new web socket connection");
+  //console.log("new web socket connection");
+
+  //welcome current user
   socket.emit("message", "welcome to ChatCord!");
+
+  //broadcast when a user connects too all except the client thats connecting
+  socket.broadcast.emit("a user has joined the chat");
+
+  //when client disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", "a user has left the chat");
+  }); //to all clients in general
 });
 
 const PORT = 3000 || process.env.PORT;
