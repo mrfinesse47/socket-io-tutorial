fef4b7b80a0221249599552e7ae80e96a5db8938
Kevin Mason <krmason2020@gmail.com>
2022-05-01 13:36:51 -0700

    now sending basic message using socket emit

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,5 @@
-const express = require("express");
-const socketio = require("socket.io");
-const http = require("http");
-const path = require("path");
+const socket = io();
 
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
-
-//set static folder
-
-app.use(express.static(path.join(__dirname, "public")));
-
-//run when a client connects
-io.on("connection", (socket) => {
-  console.log("new web socket connection");
-});
-
-const PORT = 3000 || process.env.PORT;
-
-server.listen(PORT, () => {
-  console.log(`server running on Port:${PORT}`);
+socket.on("message", (msg) => {
+  console.log(msg);
 });
